5c733d1b9427a73dd7460b0759b5094a9e348fc9
bizendaigo <200734@e0601-19.JC-21.local>
2023-06-05 10:05:10 +0900

    課題7コミットメッセージ

--- src/main/java/jp/te4a/spring/boot/myapp7/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/HelloController.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp6;
+package jp.te4a.spring.boot.myapp7;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,30 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class HelloController {
 	@Autowired
 	BookService bookService;
-	@RequestMapping("/")
+	@RequestMapping("books/list")//URL指定
 	public String index(Model model) {
 	model.addAttribute("msg", "this is setting message");
-	return "index";
+	return "books/list";//URL指定
 	}
-	@RequestMapping(value="/post", method=RequestMethod.POST)
+	@RequestMapping(value="books/list", method=RequestMethod.POST)//URL指定
 	public ModelAndView postForm(@RequestParam("id") String id, 
 	@RequestParam("title") String title,@RequestParam("writter") String writter, 
 	@RequestParam("publisher") String publisher,@RequestParam("price") String price) {
-	ModelAndView mv = new ModelAndView("index");
+	ModelAndView mv = new ModelAndView("books/list");//URL指定
 	bookService.save(new BookBean(Integer.valueOf(id), title, writter, publisher, 
 	Integer.valueOf(price)));//全パラメタ指定
-	StringBuffer buff = new StringBuffer();
-	buff.append("<HR>");
-	for(BookBean bean:bookService.findAll()) {
-		buff.append("ID:" + bean.getId() + "<BR>" + "タイトル:" + bean.getTitle() + //書籍情報を画面にセット
-		"<BR>"+ "著者:" + bean.getWritter() + "<BR>" + "出版社:" + bean.getPublisher() +// 
-		"<BR>"+ "価格:" + bean.getPrice() + "<BR><HR>");//
-	}
-	mv.addObject("msg", buff.toString());
+	mv.addObject("books",bookService.findAll());//booksとしてlist<BookBean>をそのまま渡す
 	return mv;
 	}
 }
